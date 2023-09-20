08e1aa5a30e4c388be6e9aa171abe4e6b1e15850
João Marcos <jmarcos@joywork.com.br>
2023-09-19 21:43:52 -0300

    new social media icons

--- parse.js
+++
@@ -1,7 +0,0 @@
-const getRequest = () => {
-  axios.get('http://localhost:3000')
-    .then(response => {
-      const parseInfo = response.data.data;
-    })
-    .catch(error => console.error(error));
-}  
